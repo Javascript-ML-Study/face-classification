bc5946719e372e110eea16ef543d83f5a8f60c6e
Dipokal HHJ <hhj@devent.kr>
2023-02-08 20:13:02 +0900

    feat: cut an image
    + 이미지 자르기

--- src/components/faceDetection.js
+++ src/components/faceDetection.js
@@ -1,12 +1,29 @@
+import * as tf from '@tensorflow/tfjs';
+import * as tfvis from '@tensorflow/tfjs-vis';
+
+
 class FaceDetectionElement extends HTMLElement { 
 
     constructor() {
         super()
 
+        this.inputImageSize = {
+            w: 720,
+            h: 720
+        }
+
+        this.faceImageSize = {
+            w: 32,
+            h: 32
+        }
+
         this.videoElement = undefined
         this.canvasElement = undefined
         this.canvasCtx = undefined
         this.drawingUtils = window;
+
+
+        this.test = 0
     }
 
     render() {
@@ -17,13 +34,18 @@ class FaceDetectionElement extends HTMLElement {
         this.canvasElement = this.querySelector(".output")
         this.canvasCtx = this.canvasElement.getContext('2d')
 
+        this.faceElement = this.querySelector(".output_face")
+        this.faceCtx = this.faceElement.getContext('2d')
+
+
         this.detection()
     }
 
     template() {
         return `<video class="input d-none"></video>
-        <canvas class="output" width="1280px" height="720px"></canvas>
-        <p></p>`
+        <canvas class="output" width="${this.inputImageSize.w}px" height="${this.inputImageSize.h}px"></canvas>
+        <p></p>
+        <canvas class="output_face" width="${this.faceImageSize.w}px" height="${this.faceImageSize.h}px"></canvas>`
     }
 
 
@@ -36,16 +58,22 @@ class FaceDetectionElement extends HTMLElement {
         if (results.detections.length > 0) {
             let detections = results.detections[0].boundingBox
             let location = {
-                x: (detections.xCenter - (detections.width / 2)) * 1280,
-                y: (detections.yCenter - (detections.height / 2)) * 720,
-                w: detections.width * 1280,
-                h: detections.height * 720
+                x: (detections.xCenter - (detections.width / 2)) * this.inputImageSize.w,
+                y: (detections.yCenter - (detections.height / 2)) * this.inputImageSize.h,
+                w: detections.width * this.inputImageSize.w,
+                h: detections.height * this.inputImageSize.h
             }
 
             this.querySelector("p").innerHTML = JSON.stringify(location)
-            let imageData = this.canvasCtx.getImageData(location.x, location.y, location.w, location.h);
 
-            this.canvasCtx.putImageData(imageData, 0, 0);
+            let imageData = this.canvasCtx.getImageData(location.x, location.y, location.w, location.h)
+        
+            let face = tf.browser.fromPixels(imageData)
+            let resizeFace = face.resizeBilinear([32,32]).cast('int32')
+
+            tf.browser.toPixels(resizeFace, this.faceElement);
+
+
 
             this.drawingUtils.drawRectangle(
                 this.canvasCtx, results.detections[0].boundingBox,
@@ -74,11 +102,33 @@ class FaceDetectionElement extends HTMLElement {
             onFrame: async () => {
                 await faceDetection.send({image: this.videoElement});
             },
-            width: 1280,
-            height: 720
+            width: this.inputImageSize.w,
+            height: this.inputImageSize.h
         });
 
         camera.start();
+    }
+
+
+    showVisor(imageTensor) {
+        const surface = tfvis.visor().surface({ name: 'Face Examples', tab: 'Face Data'});
+
+        surface.drawArea.appendChild(this.faceElement);
+    
+        imageTensor.dispose();
+    }
+
+    imagedataToImage(imagedata) {
+        let canvas = document.createElement('canvas');
+        let ctx = canvas.getContext('2d');
+
+        canvas.width = imagedata.width;
+        canvas.height = imagedata.height;
+        ctx.putImageData(imagedata, 0, 0);
+    
+        let image = new Image();
+        image.src = canvas.toDataURL();
+        return image;
     }
 
 
